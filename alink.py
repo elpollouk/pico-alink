46a83a2523569e8ad877053188eaedd9ba2f08d8
Adrian O'Grady <elpollouk@users.noreply.github.com>
2022-02-20 02:19:43 +0000

    Moving variables to top of file

--- alink.py
+++ alink.py
@@ -8,6 +8,27 @@ import memlog
 
 IS_MICROPYTHON = implementation.name == "micropython"
 
+# Loco function bit mapping by bank
+FUNCTIONS = {
+    0x20: (1, 2, 3, 4, 0),
+    0x21: (5, 6, 7, 8),
+    0x22: (9, 10, 11, 12),
+    0x23: (13, 14, 15, 16, 17, 18, 19, 20),
+    0x28: (21, 22, 23, 24, 25, 26, 27, 27)
+}
+
+# CV data store
+current_cv = 0
+cvs = [0] * 256
+cvs[1] = 3
+cvs[3] = 5
+cvs[4] = 5
+cvs[7] = 100
+cvs[8] = 255
+cvs[10] = 128
+cvs[29] = 6
+
+
 class Terminated(Exception):
     pass
 
@@ -39,13 +60,16 @@ def unrecognisedHandler(buffer):
     sequence = com.to_hex(buffer)
     log.warn(sequence)
 
+
 def pingHandler(_):
     log.log("Ping request")
-    com.write_with_checksum([0x62, 0x22, 0x40])
+    com.write_with_checksum((0x62, 0x22, 0x40))
+
 
 def versionHandler(_):
     log.log("Version request")
-    com.write_with_checksum([0x63, 0x21, config.DEVICE_VERSION, 0x01])
+    com.write_with_checksum((0x63, 0x21, config.DEVICE_VERSION, 0x01))
+
 
 def locoSpeedHandler(buffer):
     loco = com.read_into_buffer(buffer, 2)
@@ -63,13 +87,6 @@ def locoSpeedHandler(buffer):
     log.log(f" Speed: {speed}")
     log.log(f" Forward: {forward}")
 
-FUNCTIONS = {
-    0x20: (1, 2, 3, 4, 0),
-    0x21: (5, 6, 7, 8),
-    0x22: (9, 10, 11, 12),
-    0x23: (13, 14, 15, 16, 17, 18, 19, 20),
-    0x28: (21, 22, 23, 24, 25, 26, 27, 27)
-}
 
 def locoFunctionHandler(buffer):
     bank = buffer[-1]
@@ -97,17 +114,6 @@ def locoFunctionHandler(buffer):
 
     if buffer:
         log.log(" " + " ".join(buffer))
-
-
-current_cv = 0
-cvs = [0] * 256
-cvs[1] = 3
-cvs[3] = 5
-cvs[4] = 5
-cvs[7] = 100
-cvs[8] = 255
-cvs[10] = 128
-cvs[29] = 6
 
 
 def cvSelectHandler(buffer):
@@ -223,29 +229,29 @@ log.log("~ for debug mode")
 
 try:
     while True:
-        node = ROOT_HANDLERS
-        buffer = []
-        while True:
-            c = com.read_byte()
-            buffer.append(c)
-            node = node.get(c, unrecognisedHandler)
-            if callable(node):
-                node(buffer)
-                break
+        try:
+            node = ROOT_HANDLERS
+            buffer = []
+            while True:
+                c = com.read_byte()
+                buffer.append(c)
+                node = node.get(c, unrecognisedHandler)
+                if callable(node):
+                    node(buffer)
+                    break
 
-except Terminated:
-    pass
+        except Terminated:
+            break
 
-except Exception as ex:
-    if IS_MICROPYTHON:
-        from debug import ExceptionParser
-        parser = ExceptionParser(ex)
-        log.error(parser.details)
-        log.error(f"  {parser.function}()")
-        log.error(f"  {parser.location}")
-    else:
-        log.error(f"{type(ex).__name__}: {ex}")
-        
+        except Exception as ex:
+            if IS_MICROPYTHON:
+                from debug import ExceptionParser
+                parser = ExceptionParser(ex)
+                log.error(parser.details)
+                log.error(f"  {parser.function}()")
+                log.error(f"  {parser.location}")
+            else:
+                log.error(f"{type(ex).__name__}: {ex}")
 
 finally:
     if IS_MICROPYTHON:
